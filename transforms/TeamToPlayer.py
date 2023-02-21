de58954f1cd4aafad3981b4355d85352c0736b13
saikamat <sai.s.kamat@gmail.com>
2023-02-21 19:24:19 +0100

    Cleaned Up transform 1

--- transforms/TeamToPlayer.py
+++ transforms/TeamToPlayer.py
@@ -20,7 +20,6 @@ class TeamToPlayer(DiscoverableTransform):
     @classmethod
     def get_team_id(cls, request: MaltegoMsg):
         team_name=request.Value
-        # print(team_name) #DEBUG
         # API call to get team ID from the name
         url = "https://api-football-v1.p.rapidapi.com/v3/teams"
         querystring = {"name":{team_name}}
@@ -32,7 +31,6 @@ class TeamToPlayer(DiscoverableTransform):
       
         api_response = requests.request("GET", url, headers=headers, params=querystring)
         data = api_response.json()
-        # print(data) #DEBUG
         
         if not (data['response']):
             raise Exception("INPUT ERROR: Incorrect Input Entity Name")
@@ -43,7 +41,6 @@ class TeamToPlayer(DiscoverableTransform):
 
     @classmethod
     def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
-        print(cls.get_team_id(request)) # DEBUG
         #""""
         url = "https://api-football-v1.p.rapidapi.com/v3/players/squads"
 
@@ -56,33 +53,7 @@ class TeamToPlayer(DiscoverableTransform):
 
         api_response = requests.request("GET", url, headers=headers, params=querystring)
         data = api_response.json()
-        #"""""
-
-        """""
-        # #****** DELETE THIS CODE LATER. IT'S HERE ONLY TO SAVE THE API CALLS ******
-        # # Open a file and write the dictionary to it in JSON format
-        # with open('data/team_squads.json', 'w') as f:
-        #     json.dump(data, f)
-        
-        # # # # Close the file
-        # f.close()
-        print(data)
-
-        
-        with open('data/team_squads.json', 'r') as f:
-            data = json.load(f)
-        #****** UNTIL HERE ******
-        # print(data['response'][0]['team']['name'])
-
-        # Obtain information from source entity
-        # teamName=data['response'][0]['team']['name']
-        # print(teamName)
-        # print(data['response'][0]['players'])
-        
-        # print('*****RESPONSE.JSON')
-        # print(response1.json())
-        """""
-        #"""""
+                
         for player_id in data['response'][0]['players']:
         # Create Player Entity and add properties
             player_entity = response.addEntity("yourorganization.Player", player_id['name'])
@@ -90,5 +61,5 @@ class TeamToPlayer(DiscoverableTransform):
             player_entity.addProperty(fieldName="age", displayName="Age", value=player_id['age'])
             player_entity.addProperty(fieldName="PhotoURL", displayName="Image", value=player_id['photo'])
             player_entity.addProperty(fieldName="ID", displayName="ID", value=player_id['id'])
-        #"""""
+
         
