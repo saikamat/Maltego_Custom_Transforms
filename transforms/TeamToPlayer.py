e1f1cc188fe4ae17504d0bf5f4121eb2b3a1c602
saikamat <sai.s.kamat@gmail.com>
2023-02-21 12:28:13 +0100

    first transform version ready

--- transforms/TeamToPlayer.py
+++ transforms/TeamToPlayer.py
@@ -41,21 +41,15 @@ class TeamToPlayer(DiscoverableTransform):
         with open('data/1_PLAYER_SQ_BKUP.json', 'r') as f:
             data = json.load(f)
         #****** UNTIL HERE ******
-        print(data['response'][0]['team']['name'])
+        # print(data['response'][0]['team']['name'])
 
         # Obtain information from source entity
         teamName=data['response'][0]['team']['name']
-        player_name=data['response'][0]['players'][26]['name']
-        player_position=data['response'][0]['players'][26]['position']
-        player_age=data['response'][0]['players'][26]['age']
-        player_image_url=data['response'][0]['players'][26]['photo']
-        # print(type(player_name))
 
-        # Create Player Entity
-        player_entity = response.addEntity("yourorganization.Player", player_name)
-        player_entity.addProperty(fieldName="position", displayName="Position", value=player_position)
-        player_entity.addProperty(fieldName="age", displayName="Age", value=player_age)
-        player_entity.addProperty(fieldName="PhotoURL", displayName="Image", value=player_image_url)
-        # request_player_name = request.getProperty(player_name)
-        # print(type(request_player_name))
-        # player_entity.addProperty("Full Name", value=request_player_name) #28 is random player index
+        for player_id in data['response'][0]['players']:
+        # Create Player Entity and add properties
+            player_entity = response.addEntity("yourorganization.Player", player_id['name'])
+            player_entity.addProperty(fieldName="position", displayName="Position", value=player_id['position'])
+            player_entity.addProperty(fieldName="age", displayName="Age", value=player_id['age'])
+            player_entity.addProperty(fieldName="PhotoURL", displayName="Image", value=player_id['photo'])
+        
